31bcf0d5f734ea72578b54932c32f7aefcd82277
selimdoyranli <selim@radkod.com>
2021-03-28 02:02:40 +0300

    :pencil: update sponsoring description

--- pages/sponsorluk/index.vue
+++ pages/sponsorluk/index.vue
@@ -8,6 +8,8 @@
     br
     p Türkiye'de ne yazık ki Paypal veya Stripe gibi platformlar desteklenmiyor. Bu nedenle bağışları kripto para ödemeleri ile kabul ediyoruz. Alt tarafta yer alan butonlardan her hangi birine tıklayarak ödeme işlemini başlatabilirsin. Kripto para ödemelerinde global bir platform olan Coinbase aracılığını kullanıyoruz, ek olarak bağışçının bilgilerini elde etmek her ne kadar zor olsa da bunları tespit ettiğimiz takdirde senin bilgilerine bu sayfada oluşturacağımız sponsor listesinde yer vereceğiz.
     br
+    small Dipnot: Paribu, Btctürk gibi yerli platformlar BTC, ETH gibi yüksek hacimli coinler için düşük miktarlarda transfer desteklemez. Düşük miktarlar için LTC'yi seçebilir ya da özgür cüzdanlar üzerinden transfer yapabilirsiniz.
+    br
     donate-crypto-card-list.my-2
 </template>
 
